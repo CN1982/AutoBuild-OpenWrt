ab93bb461f275d01b03ff08e61abf404a0c98f73
CN2014 <oucezuo@qq.com>
2024-03-18 22:31:08 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
+# msd_lite
+git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
+git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 bash <(curl -Lso- https://raw.githubusercontent.com/gd0772/patch/main/x86.sh)
